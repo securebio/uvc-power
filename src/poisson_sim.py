a2cf045eea424f7a8d32f4adb082b919e3b6ad34
Daniel P. Rice <daniel.paul.rice@gmail.com>
2023-03-22 12:30:20 -0400

    Reorganize simulation running

--- src/poisson_sim.py
+++ src/poisson_sim.py
@@ -10,13 +10,14 @@ def simulate(
     Tc: int,
     p: float,
     c: float,
-    outside_infection_rate: float = 0.0,
+    outside_infection_rate: float,
+    reduction_factor: float = 1.0,
 ) -> np.ndarray:
     n_infected = np.zeros((Tc + 1, T2 - T1, num_trips))
     n_infected[0] = initialize_infected(avg_init_infected, T1, T2, num_trips)
     for i in range(1, Tc + 1):
         new_infections = generate_new_infections(
-            n_infected[i - 1], p, c, outside_infection_rate
+            n_infected[i - 1], p * reduction_factor, c, outside_infection_rate
         )
         n_infected[i, 0] = new_infections
         n_infected[i, 1:] = n_infected[i - 1, :-1]
@@ -62,85 +63,57 @@ if __name__ == "__main__":
     Tc = 10
     avg_init_infected = 16
     reduction_factor = 0.5
-    outside_infection_rate = 1.0
+    outside_infection_rate = 0.0
 
     n_sims = 10000
-    pos_tests_null1 = np.array(
-        [
-            total_positive_tests(
-                simulate(
-                    num_trips,
-                    avg_init_infected,
-                    T1,
-                    T2,
-                    Tc,
-                    p,
-                    c,
-                    outside_infection_rate,
-                ),
-                Tpos,
-            )
-            for i in range(n_sims)
-        ]
+    params = (
+        num_trips,
+        avg_init_infected,
+        T1,
+        T2,
+        Tc,
+        p,
+        c,
+        outside_infection_rate,
     )
+    sims_control = [simulate(*params) for _ in range(n_sims)]
+    sims_uv = [
+        simulate(*params, reduction_factor=reduction_factor) for _ in range(n_sims)
+    ]
 
-    pos_tests_null2 = np.array(
-        [
-            total_positive_tests(
-                simulate(
-                    num_trips,
-                    avg_init_infected,
-                    T1,
-                    T2,
-                    Tc,
-                    p,
-                    c,
-                    outside_infection_rate,
-                ),
-                Tpos,
-            )
-            for i in range(n_sims)
-        ]
+    pos_tests_control = np.array(
+        [total_positive_tests(sim, Tpos) for sim in sims_control]
     )
+    pos_tests_uv = np.array([total_positive_tests(sim, Tpos) for sim in sims_uv])
 
-    pos_tests_alt = np.array(
-        [
-            total_positive_tests(
-                simulate(
-                    num_trips,
-                    avg_init_infected,
-                    T1,
-                    T2,
-                    Tc,
-                    p * reduction_factor,
-                    c,
-                    outside_infection_rate,
-                ),
-                Tpos,
-            )
-            for i in range(n_sims)
-        ]
-    )
+    d_control = pos_tests_control - np.random.permutation(pos_tests_control)
+    d_uv = pos_tests_control - pos_tests_uv
 
-    d_null = pos_tests_null1 - pos_tests_null2
-    d_alt = pos_tests_null1 - pos_tests_alt
+    d_thresh = np.quantile(d_control, 0.95)
+    power = np.mean(d_uv > d_thresh)
 
-    d_thresh = np.quantile(d_null, 0.95)
-    power = np.mean(d_alt > d_thresh)
-    print(d_thresh)
-    print(power)
-    bins = np.arange(min(d_null) - 1, max(d_alt) + 1, 1)
-    plt.hist(d_alt[d_alt > d_thresh], bins=bins, color="C1", alpha=0.5)
+    bins = np.arange(min(d_control) - 1, max(d_uv) + 1, 1)
+    plt.hist(d_uv[d_uv > d_thresh], bins=bins, color="C1", alpha=0.5)
     plt.hist(
-        d_alt,
+        d_uv,
         bins=bins,
         histtype="step",
         color="C1",
         label=f"efficacy = {reduction_factor}",
     )
-    plt.hist(d_null, bins=bins, histtype="step", color="C0", label="efficacy = 0")
+    plt.hist(d_control, bins=bins, histtype="step", color="C0", label="efficacy = 0")
     plt.legend()
     plt.xlabel("Difference in new case numbers")
     plt.ylabel("Number of simulations")
     plt.title(f"Power = {power:.2f}")
+    plt.show()
+
+    ax1 = plt.subplot(211)
+    for sim in sims_control[:100]:
+        ax1.plot(sim[1:, Tpos, 0], color="C0", alpha=0.25)
+    ax1.set_ylim([-0.5, 10])
+    ax2 = plt.subplot(212)
+    for sim in sims_uv[:100]:
+        ax2.plot(sim[1:, Tpos, 0], color="C0", alpha=0.25)
+    ax2.set_ylim([-0.5, 10])
     plt.show()
