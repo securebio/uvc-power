d93c7e36a4b2c06e776debb597f74527c7e95a39
Daniel P. Rice <daniel.paul.rice@gmail.com>
2023-04-01 14:40:03 -0400

    Make prevalence a function of time

--- src/rig.py
+++ src/rig.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Callable
 from enum import Enum
-from random import random, randint
+from random import random
 from itertools import cycle, islice, chain
 import numpy as np
 
@@ -14,6 +14,7 @@ class Shift(str, Enum):
 class InfectionStatus(Enum):
     S = 1
     I = 2  # noqa: E741
+    R = 3
 
 
 @dataclass
@@ -50,21 +51,21 @@ def advance_day(w: Worker) -> Worker:
     return Worker(w.shift, w.shift_days + 1, w.infection_status, w.infection_days + 1)
 
 
-def recover(w: Worker, t_rec: int) -> Worker:
-    if w.infection_status == InfectionStatus.I and w.infection_days >= t_rec:
-        return Worker(w.shift, w.shift_days, InfectionStatus.S, 0)
-    else:
-        return w
-
-
 RateMap = dict[Shift, float]
 
 
 def infect(w: Worker, infection_rates: RateMap, infection_age: int = 0) -> Worker:
-    if w.infection_status == InfectionStatus.I or random() > infection_rates[w.shift]:
-        return w
-    else:
+    if w.infection_status == InfectionStatus.S and random() <= infection_rates[w.shift]:
         return Worker(w.shift, w.shift_days, InfectionStatus.I, infection_age)
+    else:
+        return w
+
+
+def recover(w: Worker, t_rec: int) -> Worker:
+    if w.infection_status == InfectionStatus.I and w.infection_days >= t_rec:
+        return Worker(w.shift, w.shift_days, InfectionStatus.R, 0)
+    else:
+        return w
 
 
 def count_shift(crew: Crew, shift: Shift) -> int:
@@ -77,10 +78,6 @@ def count_infected(crew: Crew, shift: Optional[Shift] = None) -> int:
         for w in crew
         if w.infection_status == InfectionStatus.I
     )
-
-
-def _init_infection_rates(prev: float) -> RateMap:
-    return {shift: prev for shift in Shift}
 
 
 def _generate_shift(
@@ -96,13 +93,12 @@ def _generate_shift(
     )
 
 
-def initialize_crew(
-    crew_size: int, sched: Schedule, t_change: int, prev: float, t_rec: int
-) -> Crew:
-    crew = chain.from_iterable(
-        _generate_shift(s, crew_size, sched, t_change) for s in Shift
+def initialize_crew(crew_size: int, sched: Schedule, t_change: int) -> Crew:
+    return list(
+        chain.from_iterable(
+            _generate_shift(s, crew_size, sched, t_change) for s in Shift
+        )
     )
-    return [infect(w, _init_infection_rates(prev), randint(1, t_rec)) for w in crew]
 
 
 def first_positive_test(w: Worker, t_pos: int) -> bool:
@@ -132,30 +128,30 @@ SimulationResult = list[Crew]
 def run_simulation(
     n_days: int,
     crew_size: int,
-    prevalence: float,
+    prevalence: Callable[[int], float],
     r0: float,
     t_inf: int,
     t_rec: int,
     schedule: Schedule,
     t_change: int,
 ) -> SimulationResult:
-    def _infection_rates(c: Crew) -> RateMap:
-        rate_off = prevalence / t_rec
+    def _infection_rates(c: Crew, day: int) -> RateMap:
+        rate_off = prevalence(day) / t_rec
         prop_inf_on = count_infected(c, Shift.ON) / count_shift(c, Shift.ON)
         rate_on = r0 * prop_inf_on / (t_rec - t_inf)
         return {Shift.OFF: rate_off, Shift.ON: rate_on}
 
-    def _step(c: Crew) -> Crew:
-        rates = _infection_rates(c)
+    def _step(c: Crew, day: int) -> Crew:
+        rates = _infection_rates(c, day)
         infected = (infect(w, rates) for w in c)
         recovered = (recover(w, t_rec) for w in infected)
         changed = (change_shift(w, schedule) for w in recovered)
         return [advance_day(w) for w in changed]
 
-    crew = initialize_crew(crew_size, schedule, t_change, prevalence, t_rec)
+    crew = initialize_crew(crew_size, schedule, t_change)
     sim = [crew]
-    for i in range(n_days):
-        sim.append(_step(sim[-1]))
+    for day in range(n_days):
+        sim.append(_step(sim[-1], day))
     return sim
 
 
@@ -179,7 +175,7 @@ def main():
     params = dict(
         n_days=365,
         crew_size=120,
-        prevalence=0.05,
+        prevalence=lambda d: 0.05,
         r0=1.3,
         t_inf=2,
         t_rec=12,
